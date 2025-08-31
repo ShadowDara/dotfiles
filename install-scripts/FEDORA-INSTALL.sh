2a0506204f676faf3b8e941e43b43611087f4aca
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-08-31 19:21:27 +0200

    Update FEDORA-INSTALL.sh

--- install-scripts/FEDORA-INSTALL.sh
+++ install-scripts/FEDORA-INSTALL.sh
@@ -1,17 +1,29 @@
 # Bash Script for Linux Fedora
 # to install useful stuff for programming
 
+# written by Shadowdara MIT 2025
+
+echo "Installing:"
+echo "- Neovim"
+echo "- Go"
+echo "- NodeJS"
+echo "- HTOP"
+echo "- Podman Docker"
+echo "- NixOS"
+echo "- Xournalpp"
+echo "- 7zip"
+
 # Neovim
-echo "Install Neovim with Python3 Extension"
+echo "Installing Neovim with Python3 Extension"
 sudo dnf install neovim
 sudo dnf install python3-neovim
 
 # GO
-echo "Install Golang"
+echo "Installing Golang"
 sudo dnf install golang
 
 # NodeJS
-echo "Install NodeJS"
+echo "Installing NodeJS"
 dnf module list nodejs
 sudo dnf module enable nodejs:20
 sudo dnf install nodejs
@@ -19,9 +31,22 @@ sudo dnf install nodejs
 sudo npm install -g yarn
 
 # HTOP System Monitor
-echo "Install HTOP"
+echo "Installing HTOP"
 sudo dnf install htop
 
 # Podman Docker
-echo "Install Podman Docker"
+echo "Installing Podman Docker"
 #sudo dnf install podman
+
+# NixOS
+echo "Installing NisOS"#
+sh <(curl -L https://nixos.org/nix/install) --daemon
+
+# Xournalpp
+echo "Installing Xournalpp"
+sudo dnf install xournalpp
+
+# 7zip
+echo "Installing 7zip"
+sudo dnf install p7zip p7zip-plugins
+
