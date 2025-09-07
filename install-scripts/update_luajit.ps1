68e42f62ee7b29ffa4105ceae540dd712cc9e14a
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-09-07 17:14:56 +0200

    Update update_luajit.ps1

--- install-scripts/update_luajit.ps1
+++ install-scripts/update_luajit.ps1
@@ -1,5 +1,6 @@
 # Update LuaJIT to the latest version
 # by Shadowdara 2025 MIT
 
+Remove-Item -Path "luajit.exe" -ErrorAction SilentlyContinue
 Rename-Item -Path "luajit-windows-x86_64.exe" -NewName "luajit.exe" -Force
 Write-Host "Updated LuaJIT to the latest version."
