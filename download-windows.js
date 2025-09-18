355431e37ac35050584886c5cb5871c5f393cabc
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-09-18 10:50:40 +0000

    updated it!

--- download-windows.js
+++ download-windows.js
@@ -4,4 +4,3 @@ createFolderIfNotExists('bin');
 
 // Windows Terminal Background Picture
 downloadFile("https://i.redd.it/b6khq2gmbyo51.jpg", "configs/windows-terminal/background_picture.jpg");
-downloadFile("https://github.com/PowerShell/PowerShell/releases/download/v7.4.12/PowerShell-7.4.12-win-x64.zip", "bin/PowerShell.zip");
