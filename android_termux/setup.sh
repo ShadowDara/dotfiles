8b9e350924cccb5ebd5689573907041bf8f7a61d
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-11-10 23:39:27 +0100

    Create setup.sh

---
+++ android_termux/setup.sh
@@ -0,0 +1,12 @@
+# Android Termux Install Script
+# by Shadowdara 2025
+
+pkg install git
+pkg install htop
+pkg install neovim
+pkg install python-pip
+
+# Update existing packages
+pkg upgrade
+
+echo Finished!
