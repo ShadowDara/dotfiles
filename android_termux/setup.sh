65bac2033b6609f07781e67ce2dd4df37d5f7c12
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-11-10 23:51:14 +0100

    Update setup.sh

--- android_termux/setup.sh
+++ android_termux/setup.sh
@@ -1,10 +1,26 @@
 # Android Termux Install Script
 # by Shadowdara 2025
 
+# version Control system
 pkg install git
+
+# htop task manager
 pkg install htop
+
+# neovim text editor
 pkg install neovim
+
+# pip
 pkg install python-pip
+
+# to Print system Information
+pkg install fastfetch
+
+# Download Flutter
+git clone https://github.com/flutter/flutter.git
+# Start Flutter for the First Time to download missing stuff and test it
+~/flutter/bin/flutter
+~/flutter/bin/flutter doctor
 
 # Update existing packages
 pkg upgrade
