ff66519b3f2a65a887846f7edb376389269fc216
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2025-12-10 11:08:22 +0100

    Fix setup.sh for Termux and adjust Flutter installation
    Updated setup script to correct file redirection and commented out Flutter installation steps due to ARM architecture limitations.

--- android_termux/setup.sh
+++ android_termux/setup.sh
@@ -2,7 +2,7 @@
 # by Shadowdara 2025
 
 # Create .bashrc
-cat >> datei.txt << EOF
+cat >> .bashrc << EOF
 # .bashrc File for Termux Android
 # by Shadowdara 2025
 
@@ -28,11 +28,18 @@ pkg install python-pip
 # to Print system Information
 pkg install fastfetch
 
+# INFO
+# Flutter does not work because the mobile ARM Processor Architekture.
+#
 # Download Flutter
-git clone https://github.com/flutter/flutter.git
+#git clone https://github.com/flutter/flutter.git
 # Start Flutter for the First Time to download missing stuff and test it
-~/flutter/bin/flutter
-~/flutter/bin/flutter doctor
+#~/flutter/bin/flutter
+#~/flutter/bin/flutter doctor
+
+# Install the Science Community Repo for Termux to get more packages
+pkg install x11-repo
+pkg install science-repo
 
 # Update existing packages
 pkg upgrade
