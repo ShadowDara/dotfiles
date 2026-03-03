4a3cb2027a9ad3a7fb150369dc34b8566d3c8779
ShadowDara <shadowdara74x@gmail.com>
2026-03-03 17:35:27 +0100

    Add Build Stuff

--- buildtools/download.bat
+++ buildtools/download.bat
@@ -5,3 +5,6 @@ REM Script to Download my Build Tools
 git clone https://github.com/ShadowDara/sx
 git clone https://github.com/ShadowDara/GitIgnore-Adder
 git clone https://github.com/ShadowDara/Path-Viewer
+git clone https://github.com/ShadowDara/linksaver
+git clone https://github.com/ShadowDara/tools
+git clone https://github.com/ShadowDara/script-creator
