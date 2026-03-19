7b1487804b9884e74cfa4d37bc0c6e8ae153f533
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2026-03-20 00:42:39 +0100

    Sace

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,10 +1,16 @@
 -- Syntax Highlighting
-vim.cmd("syntax on")
-vim.cmd("filetype plugin indent on")
+-- vim.cmd("syntax on")
+-- vim.cmd("filetype plugin indent on")
 
 -- Clone the Plugin Manager
 -- git clone https://github.com/LazyVim/starter ~/.config/nvim
 
 -- rm -rf ~/.config/nvim
 -- ln -s ~/dotfiles/nvim ~/.config/nvim
+
+-- Pfad zur echten LazyVim init.lua
+local lazyvim_path = vim.fn.expand("~/dotfiles/neovim/nvim/init.lua")
+
+-- Datei laden
+dofile(lazyvim_path)
 
