43ae44d34c1596c0436b9ae0692a191a1729d3d4
ShadowDara <128976697+ShadowDara@users.noreply.github.com>
2026-03-20 00:07:52 +0100

    Save

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,5 +1,10 @@
-syntax on
-filetype plugin indent on
+-- Syntax Highlighting
+vim.cmd("syntax on")
+vim.cmd("filetype plugin indent on")
 
 -- Clone the Plugin Manager
 -- git clone https://github.com/LazyVim/starter ~/.config/nvim
+
+-- rm -rf ~/.config/nvim
+-- ln -s ~/dotfiles/nvim ~/.config/nvim
+
