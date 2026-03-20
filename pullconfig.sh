f5c01d339fb36235bfd1b0d867ad22c88dea24fa
ShadowDara <shadowdara74x@gmail.com>
2026-03-20 12:54:30 +0100

    Save

--- pullconfig.sh
+++ pullconfig.sh
@@ -5,10 +5,13 @@
 # Windows Pull Stuff
 windows() {
 	echo Pulling on Windows
-	
+
 	# SX
 	cp ~/sx.conf SX
 	cp ~/sx-message.txt SX
+
+	# Applaunscher
+	cp ~/AppData/Roaming/applaunscher/applaunscher.library.json Applaunscher
 }
 
 # Linux Pull Stuff
@@ -18,8 +21,8 @@ linux() {
 	# Pull Neovim Config
 	mkdir -p neovim
 	# Using via System Links
-  rm -rf ~/.config/nvim
-  ln -s ~/dotfiles/nvim ~/.config/nvim
+    rm -rf ~/.config/nvim
+    ln -s ~/dotfiles/nvim ~/.config/nvim
 }
 
 funktion3() {
@@ -31,4 +34,9 @@ PS3="Bitte wähle eine Option: "
 select option in "Windows" "Linux" "Funktion 3"
 do
   case $REPLY in
-
+    1) windows; break ;;
+    2) linux; break ;;
+    3) funktion3 ;;
+    *) echo "Ungültig"; break ;;
+  esac
+done
