34370aad4eb000796ddba98e4b20890334396875
ShadowDara <shadowdara74x@gmail.com>
2026-03-22 19:41:36 +0100

    Save

--- pullconfig.sh
+++ pullconfig.sh
@@ -8,15 +8,33 @@ windows() {
 	echo Pulling on Windows
 
 	# SX
+  echo Pulling SX Config
 	cp ~/sx.conf SX
 	cp ~/sx-message.txt SX
 
 	# Applaunscher
+  echo Pulling Applaunscher Config
 	cp ~/AppData/Roaming/applaunscher/applaunscher.library.json Applaunscher
 
   # ShareX
+  echo Pulling ShareX Config
   cp ~/Documents/ShareX/ApplicationConfig.json ShareX
   cp ~/Documents/ShareX/HotkeysConfig.json ShareX
+
+  # VS-Code
+  echo Pulling VS-Code Config
+  cp ~/AppData/Roaming/Code/User/settings.json VS-Code
+  cp -r ~/AppData/Roaming/Code/User/snippets/ VS-Code/snippets/
+
+  # VS-Code Insiders
+  echo Pulling VS-Code Insiders Config
+  cp "$USERPROFILE/AppData/Roaming/Code - Insiders/User/settings.json" VS-Code-Insiders
+  cp -r "$USERPROFILE/AppData/Roaming/Code - Insiders/User/snippets" VS-Code-Insiders/snippets/
+
+  # Godot
+  echo Pulling Godot Config
+  mkdir -p Godot/script_templates
+  cp -r ~/AppData/Roaming/Godot/script_templates Godot/script_templates
 }
 
 
@@ -26,10 +44,12 @@ windowspush() {
   echo Push Config on Windows
 
   # Push SX
+  echo Push SX Config
   cp SX/sx.conf ~
 	cp SX/sx-message.txt ~
 
   # ShareX
+  echo Push ShareX Config
   mkdir -p ~/Documents/ShareX
   cp ShareX/ApplicationConfig.json ~/Documents/ShareX
   cp ShareX/HotkeysConfig.json ~/Documents/ShareX
@@ -47,6 +67,7 @@ linux() {
   ln -s ~/dotfiles/nvim ~/.config/nvim
 
   # Pull SX
+  echo Pull SX Config
   cp ~/sx.conf SX
 	cp ~/sx-message.txt SX
 }
@@ -58,6 +79,7 @@ linuxpush() {
   echo Push Config on Linux
 
   # Push SX
+  echo Push SX Config
   cp SX/sx.conf ~
 	cp SX/sx-message.txt ~
 }
